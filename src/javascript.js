c4009db5c55e9df76b542e95d4e5c7190dfe3160
zero-plusplus <git@zero-plusplus.com>
2020-04-17 14:12:16 +0900

    Turned off "no-magic-numbers"

--- src/javascript.js
+++ src/javascript.js
@@ -104,7 +104,7 @@ module.exports = {
     'no-labels': 'off',
     'no-lone-blocks': 'error',
     'no-loop-func': 'error',
-    'no-magic-numbers': 'error', //
+    'no-magic-numbers': 'off', // I think that it becomes difficult to read if the commonly used numbers such as 0 and -1 are restricted
     'no-multi-spaces': 'error',
     'no-multi-str': 'error',
     'no-new': 'error',

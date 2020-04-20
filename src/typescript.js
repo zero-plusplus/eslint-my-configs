708581b450fa587f77f2c6e787f8dc838b37a1ba
zero-plusplus <git@zero-plusplus.com>
2020-04-21 00:41:12 +0900

    Change option "@typescript-eslint/no-type-alias"

--- src/typescript.js
+++ src/typescript.js
@@ -84,7 +84,7 @@ module.exports = {
     '@typescript-eslint/no-require-imports': 'error',
     '@typescript-eslint/no-this-alias': 'error',
     '@typescript-eslint/no-throw-literal': 'error',
-    '@typescript-eslint/no-type-alias': 'error',
+    '@typescript-eslint/no-type-alias': [ 'error', { allowAliases: 'always' } ],
     '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
     '@typescript-eslint/no-unnecessary-condition': 'error',
     '@typescript-eslint/no-unnecessary-qualifier': 'error',

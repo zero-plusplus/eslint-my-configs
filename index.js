252672682370a3d10128bf4e648b10b16d0717f7
zero-plusplus <git@zero-plusplus.com>
2020-10-24 22:32:57 +0900

    Forget to export

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 const javascript = require('./src/javascript.js');
 const typescript = require('./src/typescript.js');
+const react = require('./src/react.js');
+const lit = require('./src/lit.js');
 
 module.exports = {
   javascript,
-  typescript, 
+  typescript,
+  react,
+  lit
 };
